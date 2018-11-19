fd0986f752596b51343aa4c0654e7c604dd991b5
Trang Luong <trang@getebm.com>
2018-11-19 15:05:22 -0500

    working script with 1 db

--- backend.py
+++ backend.py
@@ -4,10 +4,52 @@ import sqlite3
 def connect():
     conn=sqlite3.connect("files.db")
     cur=conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXIST files (id INTEGER PRIMARY KEY, client text, carrier text, status text, date text)")
+    cur.execute("CREATE TABLE IF NOT EXISTS file (id INTEGER PRIMARY KEY, client text, carrier text, status text, date text)")
     conn.commit()
     conn.close()
 
-def insert()
 
+def insert(client, carrier, status, date):
+    conn=sqlite3.connect("files.db")
+    cur=conn.cursor()
+    cur.execute("INSERT INTO file VALUES (NULL, ?,?,?,?)", (client, carrier, status, date))
+    conn.commit()
+    conn.close()
+
+
+def view():
+    conn=sqlite3.connect("files.db")
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM file")
+    rows=cur.fetchall()
+    conn.close()
+    return rows
+
+
+def search(client="", carrier="", status=""):
+    conn=sqlite3.connect("files.db")
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM file WHERE client=? OR carrier=? OR status=?",(client, carrier, status))
+    rows=cur.fetchall()
+    conn.close()
+    return rows
 connect()
+
+
+def delete(id):
+    conn=sqlite3.connect("files.db")
+    cur=conn.cursor()
+    cur.execute("DELETE FROM file WHERE id=?",(id,))
+    conn.commit()
+    conn.close()
+
+
+def update(id, client, carrier, status, date):
+    conn=sqlite3.connect("files.db")
+    cur=conn.cursor()
+    cur.execute("UPDATE file SET client=?, carrier=?, status=?, date=? WHERE id=?",(client, carrier, status, date,id))
+    conn.commit()
+    conn.close()
+
+
+
--- frontend.py
+++ frontend.py
@@ -16,8 +16,63 @@ from tkinter import *
 from datetime import date
 import backend
 
-window = Tk()
 today = date.today()
+date_text = today.strftime("%m/%d/%y")
+
+
+def get_selected_row(event):
+    try:
+        global selected_tuple
+        index=list1.curselection()[0]
+        selected_tuple=list1.get(index)
+        clear_command()
+        e1.insert(END,selected_tuple[1])
+        e2.insert(END,selected_tuple[2])
+        e3.insert(END,selected_tuple[3])
+        l5.config(text=selected_tuple[4])
+    except IndexError:
+        pass
+
+
+def view_command():
+    list1.delete(0,END)
+    for row in backend.view():
+        list1.insert(END, row)
+
+
+def clear_command():
+    e1.delete(0, 'end')
+    e2.delete(0, 'end')
+    e3.delete(0, 'end')
+
+
+def search_command():
+    list1.delete(0, END)
+    for row in backend.search(client_text.get(), carrier_text.get(), status_text.get()):
+        list1.insert(END, row)
+    clear_command()
+
+
+def add_command():
+    backend.insert(client_text.get(), carrier_text.get(), status_text.get(), date_text)
+    clear_command()
+    view_command()
+
+
+def delete_command():
+    backend.delete(selected_tuple[0])
+    view_command()
+
+
+def update_command():
+    backend.update(selected_tuple[0],client_text.get(), carrier_text.get(), status_text.get(), date_text)
+    clear_command()
+    view_command()
+
+window = Tk()
+
+window.wm_title("EDI Files")
+
 l1 = Label(window, text="Client")
 l1.grid(row=0, column=0)
 
@@ -42,7 +97,7 @@ status_text = StringVar()
 e3 = Entry(window, textvariable = status_text)
 e3.grid(row=1, column=1)
 
-date_text = today.strftime("%m/%d/%y")
+
 l5 = Label(window, text=date_text)
 l5.grid(row=1, column=3)
 
@@ -55,22 +110,25 @@ sb1.grid(row=2, column=2, rowspan=6)
 list1.configure(yscrollcommand=sb1.set)
 sb1.configure(command=list1.yview)
 
-b1 = Button(window, text="View all", width=12)
+list1.bind('<<ListboxSelect>>', get_selected_row)
+
+b1 = Button(window, text="View all", width=12, command=view_command)
 b1.grid(row=2, column=3)
 
-b2 = Button(window, text="Search entry", width=12)
+b2 = Button(window, text="Search entry", width=12, command=search_command)
 b2.grid(row=3, column=3)
 
-b3 = Button(window, text="Add entry", width=12)
+b3 = Button(window, text="Add entry", width=12, command=add_command)
 b3.grid(row=4, column=3)
 
-b4 = Button(window, text="Update", width=12)
+b4 = Button(window, text="Update", width=12, command=update_command)
 b4.grid(row=5, column=3)
 
-b5 = Button(window, text="Delete", width=12)
+b5 = Button(window, text="Delete", width=12, command=delete_command)
 b5.grid(row=6, column=3)
 
-b6 = Button(window, text="Close", width=12)
+b6 = Button(window, text="Close", width=12, command=window.destroy)
 b6.grid(row=7, column=3)
 
 window.mainloop()
+
